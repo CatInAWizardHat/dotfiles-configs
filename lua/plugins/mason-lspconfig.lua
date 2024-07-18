536a17ccec4f5f2aa2beb809a5d50decd4fc1a51
CatInAWizardHat <BeigeModel3@proton.me>
2024-07-17 19:56:57 -0700

    nvim lsps

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,10 +1,8 @@
 return {
-        "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         config = function()
-            require("mason").setup()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+               ensure_installed = { "lua_ls" }
             })
         end
 }
